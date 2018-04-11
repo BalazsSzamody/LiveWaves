ca1e7eca81970057514e3a75f6b8e56ef191d361
Fr3qu3ntFly3r <bszamody@gmail.com>
2018-04-11 10:47:03 +1000

    Initial Commit

---
+++ LiveWaves/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LiveWaves
+//
+//  Created by Szamódy Zs. Balázs on 2018. 04. 11..
+//  Copyright © 2018. Fr3qFly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
